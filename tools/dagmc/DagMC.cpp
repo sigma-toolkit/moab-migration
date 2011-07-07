sigma-toolkit/moab-migration

commit c8abd26d04cee5d44a4d03aa1ab74c63382f2332

Set geom dimension and category tags on DagMC's implicit complement
This should make the complement volume act like a "real" volume
as it would be created by ReadCGM.  In particular, it allows
entity_by_id() to be called on the implicit complement, thus
fixing an odd corner case in the DagMC interface.

Update the expected volume counts in test_geom accordingly.

Also provide a DagMC function to test if a particular handle is the
implicit complement volume.

git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@4879 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

tools/dagmc/DagMC.cpp
@@ -513,6 +513,19 @@ ErrorCode DagMC::build_obb_impl_compl(Range &surfs)
   if (MB_SUCCESS != rval)
     return rval;
   
+  // following ReadCGM, assign dimension and category tags
+  int three = 3;
+  rval = MBI->tag_set_data(geomTag, &impl_compl_handle, 1, &three );
+  if (MB_SUCCESS != rval)
+    return rval;
+
+  Tag category_tag = get_tag(CATEGORY_TAG_NAME, CATEGORY_TAG_LENGTH, 
+	  		     MB_TAG_SPARSE, MB_TYPE_OPAQUE);
+  static const char volume_category[CATEGORY_TAG_SIZE] = "Volume\0";
+  rval = MBI->tag_set_data(category_tag, &impl_compl_handle, 1, volume_category );
+  if (MB_SUCCESS != rval)
+    return rval;
+
   return MB_SUCCESS;
 
 }
@@ -1562,7 +1575,7 @@ ErrorCode DagMC::build_indices(Range &surfs, Range &vols)
     entIndices[*rit-setOffset] = idx++;
     int result=0;
     MBI->tag_get_data( idTag, &*rit, 1, &result );
-    max_id = (max_id > result ? max_id : result);
+    max_id = std::max( max_id, result ); 
   }
     // add implicit complement to entity index
   entIndices[impl_compl_handle-setOffset] = idx++ ;
@@ -1572,7 +1585,6 @@ ErrorCode DagMC::build_indices(Range &surfs, Range &vols)
   MBI->tag_set_data(idTag, &impl_compl_handle, 1, &max_id);
   
 
- 
 #ifdef CGM
   if ( have_cgm_geom ) {
     // TODO: this block should only execute if the user has explicitly requested useCAD for ray firing.
@@ -2070,6 +2082,11 @@ bool DagMC::is_white_reflect(EntityHandle surf)
 
 }
 
+bool DagMC::is_implicit_complement(EntityHandle volume)
+{
+  return volume == impl_compl_handle;
+}
+
 
 ErrorCode DagMC::write_mcnp(std::string ifile, const bool overwrite) 
 {
@@ -2127,7 +2144,7 @@ ErrorCode DagMC::write_mcnp(std::string ifile, const bool overwrite)
       if (MB_SUCCESS != rval) return rval;
       cgmfile << cellid << " " << matid << " " << density << " imp:n=" << imp ;
     }      
-    if (*iter == impl_compl_handle)
+    if ( is_implicit_complement(*iter) )
       cgmfile << "   $ implicit complement ";
     cgmfile << std::endl;
   }

tools/dagmc/DagMC.hpp
@@ -278,6 +278,7 @@ public:
   bool is_graveyard(EntityHandle volume);
   bool is_spec_reflect(EntityHandle surf);
   bool is_white_reflect(EntityHandle surf);
+  bool is_implicit_complement(EntityHandle volume);
 
   /** write metadata to temporary file for use by MCNP5 */
   ErrorCode write_mcnp(std::string ifile, const bool overwrite = true);

tools/dagmc/test_geom.cc
@@ -391,8 +391,8 @@ ErrorCode test_surface_sense( DagMC& dagmc )
   rval = moab.get_entities_by_type_and_tag( 0, MBENTITYSET, &dim_tag, ptrs+1, 1, vols );
   CHKERR;
   
-  if (vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 volume in input, found " << vols.size() << std::endl;
+  if (vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 volumes in input, found " << vols.size() << std::endl;
     return MB_FAILURE;
   }
   if (surfs.size() != 6) {
@@ -428,8 +428,8 @@ ErrorCode overlap_test_surface_sense( DagMC& dagmc )
                                             ptrs+1, 1, vols );
   CHKERR;
   
-  if (vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 volume in input, found " << vols.size() 
+  if (vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 volumes in input, found " << vols.size() 
               << std::endl;
     return MB_FAILURE;
   }
@@ -462,8 +462,8 @@ ErrorCode test_measure_volume( DagMC& dagmc )
   rval = moab.get_entities_by_type_and_tag( 0, MBENTITYSET, &dim_tag, &ptr, 1, vols );
   CHKERR;
   
-  if (vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 volume in input, found " << vols.size() << std::endl;
+  if (vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 volumes in input, found " << vols.size() << std::endl;
     return MB_FAILURE;
   }
   
@@ -494,8 +494,8 @@ ErrorCode overlap_test_measure_volume( DagMC& dagmc )
                                             &ptr, 1, vols );
   CHKERR;
   
-  if (vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 volume in input, found " << vols.size() 
+  if (vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 volumes in input, found " << vols.size() 
               << std::endl;
     return MB_FAILURE;
   }
@@ -648,8 +648,8 @@ ErrorCode test_ray_fire( DagMC& dagmc )
   rval = moab.get_entities_by_type_and_tag( 0, MBENTITYSET, &dim_tag, ptrs+1, 1, vols );
   CHKERR;
   
-  if (vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 volume in input, found " << vols.size() << std::endl;
+  if (vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 volumes in input, found " << vols.size() << std::endl;
     return MB_FAILURE;
   }
   if (surfs.size() != 6) {
@@ -773,8 +773,8 @@ ErrorCode overlap_test_ray_fire( DagMC& dagmc )
                                             ptrs+1, 1, vols );
   CHKERR;
   
-  if (vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 volume in input, found " << vols.size() 
+  if (vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 volumes in input, found " << vols.size() 
               << std::endl;
     return MB_FAILURE;
   }
@@ -893,8 +893,8 @@ ErrorCode test_point_in_volume( DagMC& dagmc )
   const void* ptr = &three;
   rval = moab.get_entities_by_type_and_tag( 0, MBENTITYSET, &dim_tag, &ptr, 1, vols );
   CHKERR;
-  if (vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 volume in input, found " << vols.size() << std::endl;
+  if (vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 volumes in input, found " << vols.size() << std::endl;
     return MB_FAILURE;
   }
   const EntityHandle vol = vols.front();
@@ -986,8 +986,8 @@ ErrorCode overlap_test_point_in_volume( DagMC& dagmc )
   rval = moab.get_entities_by_type_and_tag( 0, MBENTITYSET, &dim_tag,
                                             &ptr, 1, vols );
   CHKERR;
-  if (vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 volume in input, found " << vols.size() 
+  if (vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 volumes in input, found " << vols.size() 
               << std::endl;
     return MB_FAILURE;
   }
@@ -1059,8 +1059,8 @@ ErrorCode overlap_test_tracking( DagMC& dagmc )
                                             ptrs+1, 1, explicit_vols );
   CHKERR;
   
-  if (explicit_vols.size() != 1) {
-    std::cerr << "ERROR: Expected 1 explicit volume in input, found " 
+  if (explicit_vols.size() != 2) {
+    std::cerr << "ERROR: Expected 2 explicit volumes in input, found " 
               << explicit_vols.size() << std::endl;
     return MB_FAILURE;
   }

commit 177907677aa2c5f6d5a8179416df105dc3499478

Bugfix to implicit complement handling
Simplify build_indices() by including the implicit complement's
handle in the 'vols' parameter before calling the function.  This is
in keeping with r4879 and makes the implicit complement even more like a
"normal" volume.

This fixes a rare problem introduced by r4879, occuring when
a file with an existing implicit complement was loaded: the
complement volume would appear in the indices twice.  That
situation led to errors in dag-mcnp5. Thanks to Patrick Snouffer
for reporting the bug.

git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@5047 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

tools/dagmc/DagMC.cpp
@@ -315,6 +315,11 @@ ErrorCode DagMC::init_OBBTree()
     }
   }
 
+  // build_indices expects the implicit complement to be in vols.
+  if( vols.find(impl_compl_handle) == vols.end() ){
+    vols.insert( vols.end(), impl_compl_handle );
+  }
+
     // build the various index vectors used for efficiency
   rval = build_indices(surfs, vols);
   if (MB_SUCCESS != rval) {
@@ -1401,13 +1406,11 @@ ErrorCode DagMC::build_indices(Range &surfs, Range &vols)
   ErrorCode rval = MB_SUCCESS;
 
     // surf/vol offsets are just first handles
-  setOffset = (*surfs.begin() < *vols.begin() ? *surfs.begin() : *vols.begin());
-  setOffset = (impl_compl_handle < setOffset ? impl_compl_handle : setOffset);
+  setOffset = std::min( *surfs.begin(), *vols.begin() );
+
     // max
-  EntityHandle tmp_offset = (surfs.back() > vols.back() ? 
-                               surfs.back() : vols.back());
-  tmp_offset = (impl_compl_handle > tmp_offset ? 
-                impl_compl_handle : tmp_offset);
+  EntityHandle tmp_offset = std::max( surfs.back(), vols.back() );
+
     // set size
   rootSets.resize(tmp_offset - setOffset + 1);
   entIndices.resize(rootSets.size());
@@ -1426,19 +1429,20 @@ ErrorCode DagMC::build_indices(Range &surfs, Range &vols)
   iter = vol_handles().begin();
   *(iter++) = 0;
   std::copy( vols.begin(), vols.end(), iter );
-  vol_handles().push_back(impl_compl_handle);
+
   idx = 1;
   int max_id = -1;
   for (Range::iterator rit = vols.begin(); rit != vols.end(); rit++)    {
     entIndices[*rit-setOffset] = idx++;
-    int result=0;
-    MBI->tag_get_data( idTag, &*rit, 1, &result );
-    max_id = std::max( max_id, result ); 
-  }
-    // add implicit complement to entity index
-  entIndices[impl_compl_handle-setOffset] = idx++ ;
 
+    if( *rit != impl_compl_handle ){
+      int result=0;
+      MBI->tag_get_data( idTag, &*rit, 1, &result );
+      max_id = std::max( max_id, result ); 
+    }
+  }
     // assign ID to implicit complement
+    // for consistency with earlier versions of DagMC, make sure it always has the highest ID
   max_id++;
   MBI->tag_set_data(idTag, &impl_compl_handle, 1, &max_id);
   
@@ -1507,13 +1511,6 @@ ErrorCode DagMC::build_indices(Range &surfs, Range &vols)
   if (MB_SUCCESS != rval) return MB_FAILURE;
   for (i = 0, rit = vols.begin(); rit != vols.end(); rit++, i++)
     rootSets[*rit-setOffset] = rsets[i];
-
-  // add implicit complement root set
-  rsets.resize(1);
-  rval = MBI->tag_get_data(obb_tag(), &impl_compl_handle, 1, 
-                                       &rsets[0]);
-  if (MB_SUCCESS != rval) return MB_FAILURE;
-  rootSets[impl_compl_handle-setOffset] = rsets[0];
 
   return MB_SUCCESS;
 }
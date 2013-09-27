sigma-toolkit/moab-migration

commit d5fcdc92b644099b0b658ce80b5e6553f06f457b

Cleaning up implementation of update_remote_data and set_sharing_data.
Substantially changed structured3 to use ScdInterface-based creation of parallel mesh,
and added ghosting to this example too.

Passes all tests.

src/parallel/ParallelComm.cpp
@@ -2527,6 +2527,7 @@ ErrorCode ParallelComm::recv_entities(std::set<unsigned int>& recv_procs,
     }
 
       // add myself, if it isn't there already
+    idx = 0;
     if (new_ps[0] != (int)rank()) {
       idx = std::find(&new_ps[0], &new_ps[0] + new_numps, rank()) - &new_ps[0];
       if (idx == new_numps) {
@@ -2536,30 +2537,23 @@ ErrorCode ParallelComm::recv_entities(std::set<unsigned int>& recv_procs,
       }
       else if (!new_hs[idx] && new_numps > 2)
         new_hs[idx] = new_h;
-
-      assert(new_hs[idx] == new_h || new_numps <= 2);
-    }
-    
-      // adjust for interface layer if necessary
-    if (add_pstat & PSTATUS_INTERFACE) {
-      idx = std::min_element(&new_ps[0], &new_ps[0]+new_numps) - &new_ps[0];
-      if (idx) {
-        int tag_proc = new_ps[idx];
-        new_ps[idx] = new_ps[0];
-        new_ps[0] = tag_proc;
-        EntityHandle tag_h = new_hs[idx];
-        new_hs[idx] = new_hs[0];
-        new_hs[0] = tag_h;
-        if (new_ps[0] != (int)rank()) new_pstat |= PSTATUS_NOT_OWNED;
-      }
     }
 
-      // update for shared, multishared
+      // proc list is complete; update for shared, multishared
     if (new_numps > 1) {
       if (new_numps > 2) new_pstat |= PSTATUS_MULTISHARED;
       new_pstat |= PSTATUS_SHARED;
     }
-    
+
+      // if multishared, not ghost or interface, and not not_owned, I'm owned, and should be the first proc
+    assert(new_ps[idx] == (int)rank());
+    if ((new_numps > 2 && !(new_pstat&(PSTATUS_INTERFACE|PSTATUS_GHOST|PSTATUS_NOT_OWNED))) ||
+        (new_pstat&PSTATUS_INTERFACE && !(new_pstat&PSTATUS_NOT_OWNED))
+        ) {
+      std::swap(new_ps[0], new_ps[idx]);
+      std::swap(new_hs[0], new_hs[idx]);
+    }
+      
 /*    
     plist("new_ps", new_ps, new_numps);
     plist("new_hs", new_hs, new_numps);
@@ -2567,7 +2561,7 @@ ErrorCode ParallelComm::recv_entities(std::set<unsigned int>& recv_procs,
     std::cout << ", new_pstat=" << ostr.c_str() << std::endl;
     std::cout << std::endl;
 */
-  
+
     result = set_sharing_data(new_h, new_pstat, num_exist, new_numps, &new_ps[0], &new_hs[0]);
     RRA("update_remote_data: setting sharing data");
 
@@ -4076,7 +4070,7 @@ ErrorCode ParallelComm::resolve_shared_ents(EntityHandle this_set,
       for (v = 0; v < procs.size(); v++) {
         rval = pc[procs[v]]->update_remote_data(handles[v], 
                                                 &procs[0], &handles[0], procs.size(),
-                                                PSTATUS_INTERFACE);
+                                                (procs[0] == (int)pc[procs[v]]->rank() ? PSTATUS_INTERFACE : (PSTATUS_NOT_OWNED|PSTATUS_INTERFACE)));
         if (MB_SUCCESS != rval) return rval;
       }
     }
@@ -5919,31 +5913,16 @@ ErrorCode ParallelComm::resolve_shared_ents(EntityHandle this_set,
   {
     // set sharing data to what's passed in; may have to clean up existing sharing tags
     // if things changed too much
-  
-    ErrorCode result;
-    if (pstatus & PSTATUS_MULTISHARED && new_nump < 3) {
-      // need to remove multishared tags
-      result = mbImpl->tag_delete_data(sharedps_tag(), &ent, 1);
-      RRA("");
-      result = mbImpl->tag_delete_data(sharedhs_tag(), &ent, 1);
-      RRA("");
-      pstatus ^= PSTATUS_MULTISHARED;
-      if (new_nump < 2) 
-        pstatus = 0x0;
-      else if (ps[0] != (int)proc_config().proc_rank())
-        pstatus |= PSTATUS_NOT_OWNED;
-    }
-    else if (pstatus & PSTATUS_SHARED && new_nump < 2) {
-      hs[0] = 0;
-      ps[0] = -1;
-      pstatus = 0x0;
-    }
-
-    if (new_nump > 2) {
-      result = mbImpl->tag_set_data(sharedps_tag(), &ent, 1, ps);
-      RRA("");
-      result = mbImpl->tag_set_data(sharedhs_tag(), &ent, 1, hs);
-      RRA("");
+    
+    // check for consistency in input data
+    assert(new_nump > 1 &&
+           ((new_nump == 2 && pstatus&PSTATUS_SHARED && !(pstatus&PSTATUS_MULTISHARED)) || // if <= 2 must not be multishared
+            (new_nump > 2 && pstatus&PSTATUS_SHARED && pstatus&PSTATUS_MULTISHARED)) && // if > 2 procs, must be multishared
+           (!(pstatus&PSTATUS_GHOST) || pstatus&PSTATUS_SHARED) && // if ghost, it must also be shared
+           (new_nump < 3 || (pstatus&PSTATUS_NOT_OWNED && ps[0] != (int)rank()) || // I'm not owner and first proc not me
+            (!(pstatus&PSTATUS_NOT_OWNED) && ps[0] == (int)rank())) // I'm owner and first proc is me
+           );
+    
 #ifndef NDEBUG
       {
         // check for duplicates in proc list
@@ -5954,27 +5933,56 @@ ErrorCode ParallelComm::resolve_shared_ents(EntityHandle this_set,
         assert(dp == (int)dumprocs.size());
       }
 #endif
-      if (old_nump < 3) {
-          // reset sharedp and sharedh tags
-        int tmp_p = -1;
-        EntityHandle tmp_h = 0;
-        result = mbImpl->tag_set_data(sharedp_tag(), &ent, 1, &tmp_p);
-        RRA("");
-        result = mbImpl->tag_set_data(sharedh_tag(), &ent, 1, &tmp_h);
-        RRA("");
-      }
+
+    ErrorCode result;
+      // reset any old data that needs to be
+    if (old_nump > 2 && new_nump < 3) {
+      // need to remove multishared tags
+      result = mbImpl->tag_delete_data(sharedps_tag(), &ent, 1);
+      RRA("set_sharing_data:1");
+      result = mbImpl->tag_delete_data(sharedhs_tag(), &ent, 1);
+      RRA("set_sharing_data:2");
+//      if (new_nump < 2) 
+//        pstatus = 0x0;
+//      else if (ps[0] != (int)proc_config().proc_rank())
+//        pstatus |= PSTATUS_NOT_OWNED;
+    }
+    else if ((old_nump < 3 && new_nump > 2) || (old_nump > 1 && new_nump == 1)) {
+        // reset sharedp and sharedh tags
+      int tmp_p = -1;
+      EntityHandle tmp_h = 0;
+      result = mbImpl->tag_set_data(sharedp_tag(), &ent, 1, &tmp_p);
+      RRA("set_sharing_data:3");
+      result = mbImpl->tag_set_data(sharedh_tag(), &ent, 1, &tmp_h);
+      RRA("set_sharing_data:4");
+    }
+
+    assert("check for multishared/owner I'm first proc" &&
+           (!(pstatus & PSTATUS_MULTISHARED) || (pstatus & (PSTATUS_NOT_OWNED|PSTATUS_GHOST)) || (ps[0] == (int)rank())) &&
+           "interface entities should have > 1 proc" &&
+           (!(pstatus & PSTATUS_INTERFACE) || new_nump > 1) &&
+           "ghost entities should have > 1 proc" &&
+           (!(pstatus & PSTATUS_GHOST) || new_nump > 1)
+           );
+
+      // now set new data
+    if (new_nump > 2) {
+      result = mbImpl->tag_set_data(sharedps_tag(), &ent, 1, ps);
+      RRA("set_sharing_data:5");
+      result = mbImpl->tag_set_data(sharedhs_tag(), &ent, 1, hs);
+      RRA("set_sharing_data:6");
     }
     else {
       unsigned int j = (ps[0] == (int)procConfig.proc_rank() ? 1 : 0);
       assert(-1 != ps[j]);
       result = mbImpl->tag_set_data(sharedp_tag(), &ent, 1, ps+j);
-      RRA("");
+      RRA("set_sharing_data:7");
       result = mbImpl->tag_set_data(sharedh_tag(), &ent, 1, hs+j);
-      RRA("");
+      RRA("set_sharing_data:8");
     }
   
     result = mbImpl->tag_set_data(pstatus_tag(), &ent, 1, &pstatus);
-    RRA("");
+    RRA("set_sharing_data:9");
 
     if (old_nump > 1 && new_nump < 2) 
       sharedEnts.erase(std::find(sharedEnts.begin(), sharedEnts.end(), ent));
@@ -8996,6 +9004,13 @@ void ParallelComm::print_pstatus(unsigned char pstat, std::string &ostr)
   ppstat(PSTATUS_GHOST, "GHOST");
 
   ostr = str.str();
+}
+
+void ParallelComm::print_pstatus(unsigned char pstat) 
+{
+  std::string str;
+  print_pstatus(pstat, str);
+  std::cout << str.c_str() << std::endl;
 }
 
 } // namespace moab

src/parallel/moab/ParallelComm.hpp
@@ -664,6 +664,9 @@ namespace moab {
     //! print contents of pstatus value in human-readable form
     void print_pstatus(unsigned char pstat, std::string &ostr);
 
+    //! print contents of pstatus value in human-readable form to std::cut
+    void print_pstatus(unsigned char pstat);
+    
     // ==================================
     // \section IMESHP-RELATED FUNCTIONS
     // ==================================

test/parallel/structured3.cpp
@@ -1,242 +1,78 @@
+#include "moab/Core.hpp"
+#include "moab/ParallelComm.hpp"
+#include "moab/ScdInterface.hpp"
+#include "moab/HomXform.hpp"
+#include "TestUtil.hpp"
 #include <string>
 #include <iomanip>
 #include <iostream>
 #include <cassert>
-
-#include <moab/Core.hpp>
-#include <moab/Interface.hpp>
-#include <moab/ParallelComm.hpp>
-#include <moab/HomXform.hpp>
-#include <MBParallelConventions.h>
-#include <MBTagConventions.hpp>
-
-using namespace std;
+ 
 using namespace moab;
-
-// Number of cells in each direction:
-const int NC = 20;
+ 
+  // Number of cells in each direction:
+const int NC = 2;
 const int ITERS = 50;
-
-// Number of processes:
-const int NPROCS = 4;
-
-// Domain size:
-const double DSIZE = 10.0;
-
-// MOAB objects:
-Interface *mbint = NULL;
-ParallelComm *mbpc = NULL;
-
-// Local domain starting and ending hex indexes:
-int is, js, ks;
-int ie, je, ke;
-
-// Obvious:
-int rank;
-int size;
-
-Range all_verts;
-
-void set_local_domain_bounds();
-void create_hexes_and_verts();
-void resolve_and_exchange();
-void error(ErrorCode err);
-void tag_get_set(Tag tag);
-
+ 
+void create_parallel_mesh();
+ 
 int main(int argc, char *argv[]) 
 {
-  
-
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  if(size != 4 && size != 2) {
-    cerr << "Run this with 2 or 4 processes\n";
-    exit(1);
+  int err = RUN_TEST(create_parallel_mesh);
+ 
+  MPI_Finalize();
+  return err;
+}
+ 
+void create_parallel_mesh() 
+{
+  Core mbint;
+  ParallelComm pc(&mbint, MPI_COMM_WORLD);
+  ScdInterface *scdi;
+  ErrorCode rval = mbint.query_interface(scdi);
+  CHECK_ERR(rval);
+  pc.set_debug_verbosity(2);
+  
+    // create a structured mesh in parallel
+  ScdBox *new_box;
+  ScdParData par_data;
+  par_data.pComm = &pc;
+  par_data.gDims[0] = par_data.gDims[1] = par_data.gDims[2] = 0;
+  par_data.gDims[3] = par_data.gDims[4] = par_data.gDims[5] = NC;
+  par_data.gDims[5] = 1;
+  par_data.partMethod = ScdParData::SQIJK;
+  rval = scdi->construct_box(HomCoord(), HomCoord(), NULL, 0, // no vertex positions
+                             new_box, NULL, // not locally periodic
+                             &par_data, true, true); // assign global ids & resolve shared verts
+  CHECK_ERR(rval);
+
+  rval = pc.exchange_ghost_cells(-1, -1, 0, 0, true, true);
+  CHECK_ERR(rval);
+
+  pc.list_entities(0,-1);
+  
+  rval = pc.exchange_ghost_cells(-1, 0, 1, 0, true);
+  if (MB_SUCCESS != rval) {
+    std::string err;
+    mbint.get_last_error(err);
+    std::cerr << "Error: proc " << pc.rank() << ": " << err << std::endl;
   }
+  CHECK_ERR(rval);
 
-  mbint = new Core();
-  mbpc  = new ParallelComm(mbint, MPI_COMM_WORLD);
+  pc.list_entities(0,-1);
+  
+  return;
 
-  set_local_domain_bounds();
-  create_hexes_and_verts();
-  resolve_and_exchange();
-
-  error(mbint->get_entities_by_type(0, MBVERTEX, all_verts));
-
-
-// Create a tag
+    // Create a tag, used in exchange_tags
   Tag tag;
-  error(mbint->tag_get_handle("test_tag", 1, MB_TYPE_DOUBLE, tag, MB_TAG_DENSE|MB_TAG_EXCL));
+  int def_val = 1.0;
+  rval = mbint.tag_get_handle("test_tag", 1, MB_TYPE_DOUBLE, tag, MB_TAG_DENSE|MB_TAG_EXCL, &def_val);
+  CHECK_ERR(rval);
 
   Range empty_range;
-  tag_get_set(tag);
-
-  int i;
-  for(i = 0; i < ITERS; i++) {
-    std::cout << i << endl;
-    mbpc->exchange_tags(tag, empty_range);
-  }
-
-  delete mbpc;
-  delete mbint;
-
-  MPI_Finalize();
-  return 0;
-}
-
-void set_local_domain_bounds() 
-{
-  switch(rank) {
-    case 0:
-       
-        switch (size) {
-          case 2:
-              is = 0; ie = NC/2;
-              js = 0; je = NC;
-              ks = 0; ke = NC;
-              break;
-       
-          case 4:
-              is = 0; ie = NC/2;
-              js = 0; je = NC/2;
-              ks = 0; ke = NC;
-              break;
-        }
-        break;
-	
-    case 1:
-	
-        switch(size) {
-          case 2:
-              is = NC/2; ie = NC;
-              js = 0; je = NC;
-              ks = 0; ke = NC;
-              break;
-	
-          case 4:
-              is = NC/2; ie = NC;
-              js = 0; je = NC/2;
-              ks = 0; ke = NC;
-              break;
-        }
-        break;
-	
-    case 2:
-        is = 0; ie = NC/2;
-        js = NC/2; je = NC;
-        ks = 0; ke = NC;
-        break;
-	
-    case 3:
-        is = NC/2; ie = NC;
-        js = NC/2; je = NC;
-        ks = 0; ke = NC;
-        break;
-	
-    default:
-        cerr << "Run this with 4 processes\n";
-        exit(1);
-  }
-}
-	
-	
-void create_hexes_and_verts()
-{
-  Core *mbcore = dynamic_cast<Core*>(mbint);
-  HomCoord coord_min(0,0,0);
-  HomCoord coord_max(NC/2, NC, NC);
-  EntitySequence* vertex_seq = NULL;
-  EntitySequence* cell_seq = NULL;
-  EntityHandle vs, cs;
-	
-  error(mbcore->create_scd_sequence(coord_min, coord_max, MBVERTEX, 1, vs, vertex_seq));
-  error(mbcore->create_scd_sequence(coord_min, coord_max, MBHEX, 1, cs, cell_seq));
-	
-  HomCoord p1(0,0,0);
-  HomCoord p2(NC/2,0,0);
-  HomCoord p3(0,NC/2,0);
-	
-  error(mbcore->add_vsequence(vertex_seq, cell_seq, p1, p1, p2, p2, p3, p3));
-	
-    // Set global id's:
-  int gid;
-  Tag global_id_tag;
-  error(mbint->tag_get_handle(GLOBAL_ID_TAG_NAME, 1, MB_TYPE_INTEGER, global_id_tag));
-  EntityHandle handle = vs;
-  int i,j,k;
-	
-  ErrorCode err;
-	
-  for(i = is; i < ie + 1; i++) 
-    for(j = js; j < je + 1; j++)
-      for(k = ks; k < ke + 1; k++) {   
-        gid = k + j*(NC+1) + i*(NC+1)*(NC+1) + 1;
-        err = mbint->tag_set_data(global_id_tag, &handle, 1, &gid);
-        if(err != MB_SUCCESS) {
-          exit(1);
-        }
-        handle++;
-      }
-	
-  handle = cs;
-  for(i = is; i < ie; i++) 
-    for(j = js; j < je; j++)
-      for(k = ks; k < ke; k++) {       
-        gid = k + j*NC + i*NC*NC + 1;
-        error(mbint->tag_set_data(global_id_tag, &handle, 1, &gid));
-        handle++;
-      }
-}
-	
-	
-void resolve_and_exchange()
-{
-  EntityHandle entity_set;
-	
-    // Create the entity set:
-  error(mbint->create_meshset(MESHSET_SET, entity_set));
-	
-    // Get a list of hexes:
-  Range range;
-  error(mbint->get_entities_by_type(0, MBHEX, range));
-	
-    // Add entities to the entity set:
-  error(mbint->add_entities(entity_set, range));
-	
-    // Add the MATERIAL_SET tag to the entity set:
-  Tag tag;
-  error(mbint->tag_get_handle(MATERIAL_SET_TAG_NAME, 1, MB_TYPE_INTEGER, tag));
-  error(mbint->tag_set_data(tag, &entity_set, 1, &rank));
-	
-    // Set up partition sets. This is where MOAB is actually told what
-    // entities each process owns:
-  error(mbint->get_entities_by_type_and_tag(0, MBENTITYSET,
-                                            &tag, NULL, 1,
-                                            mbpc->partition_sets()));
-	
-    // Finally, determine which entites are shared and exchange the
-    // ghosted entities:
-  error(mbpc->resolve_shared_ents(0, -1, -1));
-  error(mbpc->exchange_ghost_cells(-1, 0, 1, 0, true));
-}
-	
-void error(ErrorCode err)
-{
-  if(err != MB_SUCCESS) {
-    cerr << "Error: MOAB function failed\n";
-    assert(0);
-  }
-}
-	
-void tag_get_set(Tag tag)
-{
-  Range::iterator iter;
-  double data;
-	
-  for(iter = all_verts.begin(); iter != all_verts.end(); iter++) {
-    data = 1.0;
-    mbint->tag_set_data(tag, &(*iter), 1, &data);
-    mbint->tag_get_data(tag, &(*iter), 1, &data);
+  for(int i = 0; i < ITERS; i++) {
+    pc.exchange_tags(tag, empty_range);
+    CHECK_ERR(rval);
   }
 }

commit aaedfd7cbeee447940e8c8db7340b9b22695c83c

Several unit tests failed on 3 or more processors, which is caused by commit d5fcdc9. This commit partially fixed the issue (unit test mpastrvpart still fails with this fix).

src/parallel/ParallelComm.cpp
@@ -2565,10 +2565,13 @@ ErrorCode ParallelComm::recv_entities(std::set<unsigned int>& recv_procs,
     if ((new_numps > 2 && !(new_pstat&(PSTATUS_INTERFACE|PSTATUS_GHOST|PSTATUS_NOT_OWNED))) ||
         (new_pstat&PSTATUS_INTERFACE && !(new_pstat&PSTATUS_NOT_OWNED))
         ) {
+      idx = std::min_element(&new_ps[0], &new_ps[0] + new_numps) - &new_ps[0];
       std::swap(new_ps[0], new_ps[idx]);
       std::swap(new_hs[0], new_hs[idx]);
+      if (new_ps[0] != (int)rank())
+        new_pstat |= PSTATUS_NOT_OWNED;
     }
-      
+
 /*    
     plist("new_ps", new_ps, new_numps);
     plist("new_hs", new_hs, new_numps);
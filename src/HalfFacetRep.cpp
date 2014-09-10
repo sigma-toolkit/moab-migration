sigma-toolkit/moab-migration

commit 7094aa67014c7928933503befc1f7b83c6ab4b87

Fix PGI warnings in src/HalfFacetRep.cpp: variable was set but never used

src/HalfFacetRep.cpp
@@ -1573,9 +1573,9 @@ namespace moab {
       for (int i = 0; i < nepf; ++i)
       {
           std::vector<EntityHandle> common(10);
-          std::vector<EntityHandle>::iterator it;
+          //std::vector<EntityHandle>::iterator it;
           if (i == nepf-1){
-              it = std::set_intersection(temp[i].begin(), temp[i].end(), temp[0].begin(), temp[0].end(), common.begin());
+              std::set_intersection(temp[i].begin(), temp[i].end(), temp[0].begin(), temp[0].end(), common.begin());
               if (*common.begin() == 0)
                   continue;
 
@@ -2480,12 +2480,12 @@ namespace moab {
       for (int i = 0; i < nepc; ++i)
       {
           std::vector<EntityHandle> common(10);
-          std::vector<EntityHandle>::iterator it;
+          //std::vector<EntityHandle>::iterator it;
 
           int lv0 = lConnMap3D[index].e2v[i][0];
           int lv1 = lConnMap3D[index].e2v[i][1];
 
-          it = std::set_intersection(temp[lv0].begin(), temp[lv0].end(), temp[lv1].begin(), temp[lv1].end(), common.begin());
+          std::set_intersection(temp[lv0].begin(), temp[lv0].end(), temp[lv1].begin(), temp[lv1].end(), common.begin());
           if (*common.begin() == 0)
               continue;
 

commit 8b59a0d7a284d1363c72bbf8e480b56a829c6ab1

Fix a complier error in src/HalfFacetRep.cpp (caused by commit 7094aa6)

src/HalfFacetRep.cpp
@@ -1583,7 +1583,7 @@ namespace moab {
           }
           else
           {
-              it = std::set_intersection(temp[i].begin(), temp[i].end(), temp[i+1].begin(), temp[i+1].end(), common.begin());
+              std::set_intersection(temp[i].begin(), temp[i].end(), temp[i+1].begin(), temp[i+1].end(), common.begin());
               if (*common.begin() == 0)
                   continue;
 
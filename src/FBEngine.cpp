sigma-toolkit/moab-migration

commit 80f4cade72a2f54132d1d3bab55ad147267b8a22

Fix more warnings
git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@5318 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

src/FBEngine.cpp
@@ -2346,14 +2346,12 @@ ErrorCode FBEngine::split_edge_at_mesh_node(EntityHandle edge, EntityHandle node
   // find the edge connected to the splitting node
   int num_mesh_edges = (int)ents.size();
   int index_edge;
-  EntityHandle firstNode;
+  EntityHandle firstNode = conn[0]; // will be used to decide vertex sets adjacencies
   for (index_edge = 0; index_edge<num_mesh_edges; index_edge++)
   {
     rval = MBI->get_connectivity(ents[index_edge], conn, len);
     if (MB_SUCCESS != rval)
       return rval;
-    if (index_edge == 0)
-      firstNode = conn[0];// will be used to decide vertex sets adjacencies
     if (conn[0] == node)
     {
       if (index_edge==0)

src/ReorderTool.cpp
@@ -551,6 +551,7 @@ ErrorCode ReorderTool::reorder_tag_data( EntityType type, Tag new_handles, Tag t
       case MB_TYPE_HANDLE:  type_size = sizeof(EntityHandle); break;
       case MB_TYPE_BIT:     type_size = 1; break;
       case MB_TYPE_OPAQUE:  type_size = 1; break;
+      default:              return MB_TYPE_OUT_OF_RANGE;
     }
     buffer.resize( total*type_size );
     size_t off = 0;

src/io/WriteNCDF.cpp
@@ -533,7 +533,7 @@ ErrorCode WriteNCDF::gather_mesh_information(
     const void* ptr = 0;
    
     int has_dist_factors = 0; 
-    if(mdbImpl->tag_get_data(mDistFactorTag,&(*vector_iter), 1, &ptr, &dist_factor_size) == MB_SUCCESS &&
+    if(mdbImpl->tag_get_by_ptr(mDistFactorTag,&(*vector_iter), 1, &ptr, &dist_factor_size) == MB_SUCCESS &&
        dist_factor_size)
       has_dist_factors = 1;
     dist_factor_size /= sizeof(double);
@@ -626,8 +626,8 @@ ErrorCode WriteNCDF::get_valid_sides(Range &elems, ExodusMeshInfo& /*mesh_info*/
   const double* dist_fac_iter = 0;
   const void* ptr = 0;
   bool has_dist_factors = false; 
-  if(mdbImpl->tag_get_data(mDistFactorTag,
-                           &(sideset_data.mesh_set_handle), 1, &ptr, &dist_factor_size) == MB_SUCCESS &&
+  if(mdbImpl->tag_get_by_ptr(mDistFactorTag,
+                             &(sideset_data.mesh_set_handle), 1, &ptr, &dist_factor_size) == MB_SUCCESS &&
      dist_factor_size)
   {
     has_dist_factors = true;

commit fd8df7c79e57ac236001bf21eb145285fc30e092

Fixed a null pointer dereference bug (reported by cppcheck). This bug was introduced in commit 80f4cad which tried to fix some warnings (e.g. uninitialized variable).

src/FBEngine.cpp
@@ -2361,12 +2361,14 @@ ErrorCode FBEngine::split_edge_at_mesh_node(EntityHandle edge, EntityHandle node
   // find the edge connected to the splitting node
   int num_mesh_edges = (int)ents.size();
   int index_edge;
-  EntityHandle firstNode = conn[0]; // will be used to decide vertex sets adjacencies
+  EntityHandle firstNode = 0;
   for (index_edge = 0; index_edge<num_mesh_edges; index_edge++)
   {
     rval = MBI->get_connectivity(ents[index_edge], conn, len);
     if (MB_SUCCESS != rval)
       return rval;
+    if (index_edge == 0)
+      firstNode = conn[0]; // will be used to decide vertex sets adjacencies
     if (conn[0] == node)
     {
       if (index_edge==0)